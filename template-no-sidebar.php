62fa63f1211ffa219e5a6ea35089f487de0f2de3
dpeters911 <dpeters911@hotmail.com>
2018-01-27 15:31:44 -0800

    Page Template Titling
    Overhaul to page templates: template-full-width and template-no-sidebar now use a featured post img.

--- template-no-sidebar.php
+++ template-no-sidebar.php
@@ -5,23 +5,18 @@ Template Name: Page (No Sidebar)
 
 get_header(); ?>
 	
-	<div class="content grid-container">
+	<div class="content grid-container page-no-sidebar">
 	
 		<div class="inner-content grid-x grid-margin-x grid-padding-x">
 	
 		    <main class="main small-12 large-12 medium-12 cell" role="main">
 				
-				<?php if ( has_post_thumbnail() ) {
-				    the_post_thumbnail( 'page-hero' );
-				}
-				else {
-				    echo '';
-				}
-				?>
-
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-			    	<?php get_template_part( 'parts/loop', 'page' ); ?>
+					<header class="article-header" style="background-image: url('<?php if ( has_post_thumbnail() ) { the_post_thumbnail_url( 'full' ); } else { echo ''; } ?> ')">
+							<h1 class="page-title"><div class="grid-container"><?php the_title(); ?></div></h1>
+					</header>
+			    	<?php get_template_part( 'parts/loop', 'page-full' ); ?>
 			    
 			    <?php endwhile; endif; ?>							
 			    					
